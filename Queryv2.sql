5da32a55ab55bdeea5580ba6265a05d8626de009
Jordan Benner <jordanbenner@outlook.com>
2017-05-17 10:25:04 -0500

    Problem 3

--- Queryv2.sql
+++ Queryv2.sql
@@ -2,3 +2,5 @@
 SELECT * FROM review WHERE restaurant_id = 5;
 --Problem 2
 SELECT review.review FROM review, restaurant WHERE restaurant_id = restaurant.id AND restaurant.name = 'moon tower';
+--Problem
+SELECT review.review FROM review, reviewer WHERE reviewer_id = reviewer.id AND reviewer.name = 'silent bob';
