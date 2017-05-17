bde64a84eb5cbf8061b45e4fd48b66a6082f32c7
Jordan Benner <jordanbenner@outlook.com>
2017-05-17 10:57:57 -0500

    Problem 5

--- Queryv2.sql
+++ Queryv2.sql
@@ -6,3 +6,5 @@ SELECT review.review FROM review, restaurant WHERE restaurant_id = restaurant.id
 SELECT review.review FROM review, reviewer WHERE reviewer_id = reviewer.id AND reviewer.name = 'silent bob';
 --Problem 4
 SELECT review.review, restaurant.name FROM review, restaurant WHERE restaurant_id = restaurant.id;
+--Problem 5
+SELECT restaurant.id, restaurant.name, restaurant.address, AVG(review.stars) FROM review, restaurant WHERE review.restaurant_id = restaurant.id GROUP BY restaurant.id;
